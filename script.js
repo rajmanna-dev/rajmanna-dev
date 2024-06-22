a50902f2ef4533ce5e11cb7a8b3db3309f15f2a2
Raj Manna <raj2021manna@gmail.com>
2024-06-22 20:18:03 +0530

    fix color schema

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
+  // Handle Navbar during scrolling
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $('nav').css('background-color', '#274d79');
+    } else {
+      $('nav').css('background-color', '');
+    }
+  });
+
+  // Set Color to the Navbar Menus then Someone Click on it
   $('.menu').on('click', function () {
     $('.menu').removeClass('activeMenu');
     $(this).addClass('activeMenu');
   });
 
+  // Smooth Scroll
   $('a').on('click', function (event) {
     if (this.hash !== '') {
       event.preventDefault();
@@ -21,25 +32,25 @@ $(document).ready(function () {
     }
   });
 
+  // Handle Mouse Hover on the Portfolios Section
   $('.portfolio').on('mouseenter', function () {
     $(this).find('.overlay').removeClass('hidden');
     $(this)
       .find('.overlay')
-      .addClass('flex flex-col items-center justify-center');
+      .addClass('flex')
+      .css('transition', 'all ease-in-out 0.8s');
   });
 
   $('.portfolio').on('mouseleave', function () {
-    $(this)
-      .find('.overlay')
-      .removeClass('flex flex-col items-center justify-center');
     $(this).find('.overlay').addClass('hidden');
+    $(this).find('.overlay').removeClass('flex');
   });
 
   if ($(window).width() <= 768) {
     $('.portfolio').on('click', function () {
       $(this)
         .find('.overlay')
-        .css('background-color', 'rgba(250, 204, 21, 0.78)');
+        .css('background-color', 'rgba(39, 77, 121, 0.84)');
     });
   }
 });
